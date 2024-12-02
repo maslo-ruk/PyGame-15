3f2c83fbfd4e20790ad10bd2c93d2ddf067e1ada
Жаворонков Егор <maslo.paslo@yandex.ru>
2024-12-02 16:55:13 +0300

    commit third dude1

--- papka1/main.py
+++ papka1/main.py
@@ -2,7 +2,7 @@ def print_hi(name):
     print(f'Hi, {name}')
     print('develop branch')
     print('w')
-    #I am second dude
+    #I am third dude
     print('second_dude+things')
 
 if __name__ == '__main__':
