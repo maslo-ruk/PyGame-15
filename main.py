5d9bef42afd71c9dadbff52e9ad6614c56c3ba50
Жаворонков Егор <maslo.paslo@yandex.ru>
2024-12-02 17:06:20 +0300

    commit second dude2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+    print('dcs')
 
 
 if __name__ == '__main__':
