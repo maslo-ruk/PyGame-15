85f04b7813c191a27f249d894370704fce67848a
Жаворонков Егор <maslo.paslo@yandex.ru>
2024-12-02 16:32:15 +0300

    dev commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+    print(f'Hi, {name}')
+    print('develop branch')
 
 
 if __name__ == '__main__':
